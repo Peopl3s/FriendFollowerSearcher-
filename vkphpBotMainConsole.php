50fdfd8116d7f30fc312adf2d0916f40074cb99e
Peopl3s <nikposd@mail.ru>
2020-03-20 13:20:48 +0300

    Changed the location of functions in the file

--- vkphpBotMainConsole.php
+++ vkphpBotMainConsole.php
@@ -1,27 +1,6 @@
 <?php
 require_once 'vkphpbot.php';
 
-function write_data_in_file($data, $file_name = 'friend_follower.txt')
-{
-	$fd = fopen($file_name, 'a') or die("Failed to create a file");
-	fwrite($fd, "\r\n");
-	foreach($data as $element)
-	{
-		fwrite($fd, $element."\r\n");
-	}
-	fclose($fd);
-}
-
-function is_have_friends_and_followers($friends, $followers)
-{
-	return $friends != '' and $followers != '';
-}
-
-function is_correct_vk_user_id($vk_user_id)
-{
-	return $vk_user_id != '' and preg_match('/(https:\/\/vk.com\/)?(id\/)?([0-9a-z_]+)/',$vk_user_id); 
-}
-
 fwrite(STDOUT, "Enter the VK pade id: ");
 
 $vk_user_id = trim(fgets(STDIN)); 
@@ -43,4 +22,26 @@ if(is_correct_vk_user_id($vk_user_id))
 		fwrite(STDOUT, "The user is unavailable".PHP_EOL);
 	}
 }
+
+function is_correct_vk_user_id($vk_user_id)
+{
+	return $vk_user_id != '' and preg_match('/(https:\/\/vk.com\/)?(id\/)?([0-9a-z_]+)/',$vk_user_id); 
+}
+
+function is_have_friends_and_followers($friends, $followers)
+{
+	return $friends != '' and $followers != '';
+}
+
+function write_data_in_file($data, $file_name = 'friend_follower.txt')
+{
+	$fd = fopen($file_name, 'a') or die("Failed to create a file");
+	fwrite($fd, "\r\n");
+	foreach($data as $element)
+	{
+		fwrite($fd, $element."\r\n");
+	}
+	fclose($fd);
+}
+
 ?>
